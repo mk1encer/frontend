f3f98948b21bc3c4f38f226440ffb276853d4fc3
mk1encer <mksur1er@gmail.com>
2022-08-05 17:44:24 +0900

    first commit

---
+++ src/components/HomePage/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import MainContent from "../MainContent/MainContent";
+import NavBar from "../NavBar/NavBar";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div>
+      <NavBar />
+      <MainContent />
+    </div>
+  );
+}
+
+export default Home;
