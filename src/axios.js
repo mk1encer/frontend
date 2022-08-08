195eadfe4c9110e85019f18fb903db8d3e47c8b4
mk1encer <mksur1er@gmail.com>
2022-08-08 16:07:03 +0900

    add for json

--- src/axios.js
+++ src/axios.js
@@ -1,21 +1,35 @@
 import client from "./client";
 
+const customConfig = {
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
 export const SignIn = ({ email, password }) => {
   console.log("Signin");
-  client.post("auth-service/login", {
-    email,
-    password,
-  });
+  client.post(
+    "auth-service/login",
+    JSON.stringify({
+      email,
+      password,
+    }),
+    customConfig
+  );
 };
 
 export const SignUp = ({ email, fullname, username, password }) => {
   console.log("signup");
-  client.post("./auth-service/register", {
-    email,
-    fullname,
-    username,
-    password,
-  });
+  client.post(
+    "./auth-service/register",
+    JSON.stringify({
+      email,
+      fullname,
+      username,
+      password,
+    }),
+    customConfig
+  );
 };
 
 export const getUser = ({ userId }) =>
