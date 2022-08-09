b53d711d4e3c3edbf43f7adae5aa724fd4b0fb54
mk1encer <mksur1er@gmail.com>
2022-08-09 12:24:51 +0900

    add validation check

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -8,10 +8,12 @@ import {
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "../LoginPage/LoginPage.css";
+import { toHaveErrorMessage } from "@testing-library/jest-dom/dist/matchers";
 
 const SignUp = () => {
   const dispatch = useDispatch();
   const [error, setError] = useState(null);
+
   const { form, auth, authError, checkedEmail, checkedNickname } = useSelector(
     ({ auth }) => ({
       form: auth.SignUp,
@@ -21,6 +23,39 @@ const SignUp = () => {
       checkedNickname: auth.checkedNickname,
     })
   );
+
+  const validate = (form, errors) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+
+    if (!form.email || form.email === null) {
+      errors.email = "이메일을 입력해주세요";
+    } else if (!regex.test(form.email)) {
+      errors.email = "이메일 형식이 아닙니다";
+    } else if (!checkedEmail) {
+      errors.email = "이메일이 중복입니다";
+    }
+
+    if (!form.name || form.name === null) {
+      errors.name = "이름을 입력해주세요";
+    } else if (form.name.length < 2 || form.name.length > 16) {
+      errors.name = "이름은 2~16자리로 입력해주세요";
+    }
+
+    if (!form.nickname || form.nickname === null) {
+      errors.nickname = "닉네임을 입력해주세요";
+    } else if (form.nickname.length < 2 || form.nickname.length > 16) {
+      errors.nickname = "닉네임은 2~16자리로 입력해주세요";
+    } else if (!checkedNickname) {
+      errors.nickname = "닉네임이 중복입니다";
+    }
+
+    if (!form.password || form.password === null) {
+      errors.password = "비밀번호를 입력해주세요";
+    } else if (form.password.length < 2 || form.password.length > 16) {
+      errors.password = "비밀번호는 2~16자리로 입력해주세요";
+    }
+    return errors;
+  };
 
   const onChange = (e) => {
     const { value, name } = e.target;
@@ -37,62 +72,25 @@ const SignUp = () => {
     e.preventDefault();
 
     const { email, name, nickname, password } = form;
+    let errors = {};
 
-    if ([email, name, nickname, password].includes("")) {
-      setError("모든 칸을 채워주세요");
-      console.log("모든 칸을 채워주세요");
-
+    if (Object.keys(validate(form, errors)).length !== 0) {
+      alert("유효하지 않은 항목이 있습니다");
       return;
     }
-
-    if (checkedEmail && checkedNickname) {
-      dispatch(
-        signup({
-          email,
-          name,
-          nickname,
-          password,
-        })
-      );
-    }
-
-    // dispatch(
-    //   signup({
-    //     email,
-    //     name,
-    //     nickname,
-    //     password,
-    //   })
-    // );
+    dispatch(
+      signup({
+        email,
+        name,
+        nickname,
+        password,
+      })
+    );
   };
 
   useEffect(() => {
     dispatch(initializeForm("signup"));
   }, [dispatch]);
-
-  useEffect(() => {
-    const { email } = form;
-
-    dispatch(checkEmail(email));
-
-    if (!checkedEmail) {
-      setError("이메일 중복!");
-
-      return;
-    }
-  }, [dispatch, checkedEmail, form]);
-
-  useEffect(() => {
-    const { nickname } = form;
-
-    dispatch(checkNickname(nickname));
-
-    if (!checkedNickname) {
-      setError("닉네임 중복!");
-
-      return;
-    }
-  }, [dispatch, checkedNickname, form]);
 
   return (
     <div>
